61c675f77aa141c97a5946e836099875a10d841b
ABCDullahh <betason123@gmail.com>
2022-10-28 14:12:40 +0700

    PPB mod4

---
+++ common/Data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+    id: 1,
+    nama: "OSVELINA MARGARETHA SIREGAR",
+    nim: "21120120140167",
+    kelompok: "20"
+    },
+    {
+    id: 2,
+    nama: "HAMMAN FAIZ",
+    nim: "21120120140076",
+    kelompok: "20"
+    },
+    {
+    id: 3,
+    nama: "MUHAMMAD RIZAL QOWI AL-AZIS",
+    nim: "21120120140172",
+    kelompok: "20"
+    },
+    {
+    id: 4,
+    nama: "FAIZAL LUTFI YOGA TRIADI",
+    nim: "21120120140158",
+    kelompok: "20"
+    }
+];
